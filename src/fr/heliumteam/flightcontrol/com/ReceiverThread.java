5d934a5538845a0c34807a7a4d808748f65a4698
Karang <arthur.hennequin@free.fr>
2014-03-05 19:18:52 +0100

    Add low pass filter for battery monitor

--- src/fr/heliumteam/flightcontrol/com/ReceiverThread.java
+++ src/fr/heliumteam/flightcontrol/com/ReceiverThread.java
@@ -6,10 +6,12 @@ import java.io.InputStream;
 import fr.heliumteam.flightcontrol.GroundControl;
 import fr.heliumteam.flightcontrol.tools.BatteryTool;
 import fr.heliumteam.flightcontrol.tools.ByteTool;
+import fr.heliumteam.flightcontrol.tools.MathHelper;
 
 public class ReceiverThread extends Thread {
 	
 	private InputStream input;
+	private float previousVCC = 0;
 	
 	public ReceiverThread(InputStream input) {
 		this.input = input;
@@ -25,8 +27,6 @@ public class ReceiverThread extends Thread {
 				byte[] data = new byte[16];
 				input.read(data);
 
-				//ByteTool.printBytes(data);
-
 				float yaw, pitch, roll, batterie;
 
 				yaw = ByteTool.byteToFloat(data[0], data[1], data[2], data[3]);
@@ -34,15 +34,15 @@ public class ReceiverThread extends Thread {
 				roll = ByteTool.byteToFloat(data[8], data[9], data[10], data[11]);
 				batterie = ByteTool.byteToFloat(data[12], data[13], data[14], data[15]);
 
-				//GroundControl.getGCS().log("Recu : "+batterie);
-				//ByteTool.printBytes(new byte[]{data[0], data[1], data[2], data[3]});
+				batterie *= 3.2f; // Diviseur de tension
+				batterie = MathHelper.lerp(batterie, previousVCC, 0.5f); // Low pass filter
+				previousVCC = batterie;
 				
 				GroundControl.getGCS().getYaw().setYaw(yaw);
 				GroundControl.getGCS().getPitchRoll().setPitch(pitch);
 				GroundControl.getGCS().getPitchRoll().setRoll(roll);
-				GroundControl.getGCS().getBatterieVolt().setValue(batterie*3.2f);
+				GroundControl.getGCS().getBatterieVolt().setValue(batterie);
 				GroundControl.getGCS().getBatterie().setValue(BatteryTool.getPercent(batterie));
-				//GroundControl.getGCS().log("Vcc : "+batterie);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
